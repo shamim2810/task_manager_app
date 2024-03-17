4f297fb9d65e7c47d16e0a0015d963ba98e7b19e
shamim2810 <shamim149120@gmail.com>
2024-03-17 23:10:44 +0600

    UI Design IV

--- lib/presentation/widgets/profile_app_bar.dart
+++ lib/presentation/widgets/profile_app_bar.dart
@@ -1,55 +1,69 @@
+import 'dart:js';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:task_manager_app/app.dart';
 import 'package:task_manager_app/presentation/screens/auth/sign_in_screen.dart';
+import 'package:task_manager_app/presentation/screens/update_profile_screen.dart';
 import 'package:task_manager_app/presentation/utilis/app_colors.dart';
 
 PreferredSizeWidget get profileAppBar {
   return AppBar(
+    automaticallyImplyLeading: false,
     backgroundColor: AppColors.themeColor,
-    title: Row(
-      children: [
-        const CircleAvatar(),
-        const SizedBox(
-          width: 12,
-        ),
-        const Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Rabbil Hasan',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                'rabbil@gmail.com',
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                ),
-              )
-            ],
+    title: GestureDetector(
+      onTap: () {
+        Navigator.push(
+          TaskManager.navigatorKey.currentState!.context,
+          MaterialPageRoute(
+            builder: (context) => const UpdateProfileScreen(),
           ),
-        ),
-        IconButton(
-          onPressed: () {
-            Navigator.pushAndRemoveUntil(
-                TaskManager.navigatorKey.currentState!.context,
-                MaterialPageRoute(
-                  builder: (context) => const SignInScreen(),
-                ),
-                (route) => false);
-          },
-          icon: const Icon(
-            Icons.logout,
-            color: Colors.white,
+        );
+      },
+      child: Row(
+        children: [
+          const CircleAvatar(),
+          const SizedBox(
+            width: 12,
           ),
-        ),
-      ],
+          const Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Rabbil Hasan',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'rabbil@gmail.com',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                )
+              ],
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.pushAndRemoveUntil(
+                  TaskManager.navigatorKey.currentState!.context,
+                  MaterialPageRoute(
+                    builder: (context) => const SignInScreen(),
+                  ),
+                  (route) => false);
+            },
+            icon: const Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
     ),
   );
 }
